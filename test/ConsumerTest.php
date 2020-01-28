0b94262bacdba0575720721148860d0232eff6ee
Mark Bradley <braddle@gmail.com>
2020-01-28 19:48:09 +0000

    Implementing the HTTP Producer

--- test/ConsumerTest.php
+++ test/ConsumerTest.php
@@ -1,9 +1,25 @@
 <?php
 namespace Braddle;
 
+use GuzzleHttp\Psr7\Uri;
+use PhpPact\Standalone\ProviderVerifier\Model\VerifierConfig;
+use PhpPact\Standalone\ProviderVerifier\Verifier;
 use PHPUnit\Framework\TestCase;
 
 class ConsumerTest extends TestCase
 {
+    public function testPersonConsumer()
+    {
+        $config = new VerifierConfig();
+        $config->setProviderName("personProvider")
+            ->setProviderVersion("1.0.0")
+            ->setProviderBaseUrl(new Uri("http://localhost:8080"))
+            ->setPublishResults(true);
+
+        $verifier = new Verifier($config);
+        $verifier->verifyFiles([__DIR__ . ])
+
+        $this->assertTrue(true);
+    }
 
 }
--- web/index.php
+++ web/index.php
@@ -1,11 +1,24 @@
 <?php
 
 use Slim\Factory\AppFactory as AppFactoryAlias;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $app = AppFactoryAlias::create();
 $app->addErrorMiddleware(false, true, false);
 
+$app->get("/person/{id}", function (Request $request, Response $response, array $args) {
+    $body = [
+        "first_name" => "Dade",
+        "last_name" => "Murphy",
+        "alias" => "Zero Cool",
+        "age" => 26,
+    ];
+    $response = $response->withHeader("Content-Type", "application/json");
+    $response->getBody()->write(json_encode($body));
+    return $response;
+});
 
 $app->run();
